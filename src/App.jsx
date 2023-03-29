da31a579c57fffcfa8f0e7e51c08ca034ef46b54
Dakota McPherson <dakotalmcpherson@gmail.com>
2023-03-29 15:52:16 -0400

    created components for object visualization

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import styles from './App.module.css';
 import { QueryClient, QueryClientProvider, createQuery, QueryCache, useQueryClient } from '@tanstack/solid-query'
 import { For, useContext } from 'solid-js';
 import PokeName from './PokeName';
+import { ObjectComponent } from './JSONComponents/ObjectComponent';
+import { Header } from './Header'
 
 
 
@@ -15,10 +17,23 @@ function App() {
     return data.results;
   })
 
+  const testObj = {
+    name: 'dakota',
+    age: 28,
+    cool: true,
+    lilBit: true,
+    somethingElse: undefined,
+    testNull: null,
+    arrTest: [1,2,3],
+    arr2Test: ['test', 3, null, true, {'please': 'work'},{'hello': 'bye'}]
+  }
+
   return (
     <>
+    <Header name={'Data Explorer'} />
+    <ObjectComponent obj={testObj} key={"testObj"} level={0} />
      <For each={query.data}>
-         {(pokemon, i) => 
+         {(pokemon, i) =>
         <PokeName name={pokemon.name} url={pokemon.url} num={i() + 1}/>
           }
       </For>
