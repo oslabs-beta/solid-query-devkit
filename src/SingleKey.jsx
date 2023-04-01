53759942b50773dd242d8b59ee0e5a4affbf71e3
Dakota McPherson <dakotalmcpherson@gmail.com>
2023-04-01 14:31:31 -0400

    working button on top right

--- src/SingleKey.jsx
+++ src/SingleKey.jsx
@@ -7,20 +7,15 @@ export default function SingleKey(props)   {
   const queryClient = useQueryClient();
   const {activeQuery, setActiveQuery} = useContext(QueryContext)
   const {queries, setQueries} = useContext(QueryContext)
-
-  const status = () => props.status;
+  
+  const query = () => (queries().filter((query) => query.queryHash === props.key)[0])
 
   return (
     <div onClick={() => {
-      
-      setActiveQuery(queries().filter(obj => obj.queryHash == props.queryHash)[0])
-      console.log('acitve query: ', activeQuery())
-      // console.log(queryClient.getQueryCache().queries)
+      setActiveQuery(query())
+      console.log('acitve query:', activeQuery())
     }}>
-      <span>Observers: {props.numOfObservers}</span>
-      <span>Key: {props.queryKey}</span>
-      <span>Status: {status()}</span>
-      <span>Index: {props.index}</span>
+      <span>{query().state.status || 'undefined'}</span>
     </div>
   );
 };
