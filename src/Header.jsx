024c68bec4d43d681bd2e1588dadc36ab808df82
Truett <truett.davis@gmail.com>
2023-04-07 11:02:08 -0400

    progress on status indicator border

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,19 +5,29 @@ import logo from "./assets/SquidLogo.png";
   //Style Variables for Status Backgrounds: 
   //Loading
   const noneLoading = { "background-color": "rgb(14, 46, 96)", "color": "rgb(89, 98, 109)" };
+  const noneLoadingFiltered = { "background-color": "rgb(14, 46, 96)", "color": "rgb(89, 98, 109)", "border-color": "white", "border-style": "solid" };
   const someLoading = { "background-color": "blue", "color": "white" };
+  const someLoadingFiltered = { "background-color": "blue", "color": "white", "border-color": "white", "border-style": "solid" };
   //Fresh
   const noneFresh = { "background-color": "rgb(16, 66, 53)", "color": "rgb(89, 98, 109)" };
+  const noneFreshFiltered = { "background-color": "rgb(16, 66, 53)", "color": "rgb(89, 98, 109)", "border-color": "white", "border-style": "solid" };
   const someFresh = { "background-color": "green", "color": "white" };
+  const someFreshFiltered = { "background-color": "green", "color": "white", "border-color": "white", "border-style": "solid" };
   //Paused
-  const nonePaused = {"background-color": "rgb(140, 73, 235)", "color": "white", "opacity": "0.3"};
-  const somePaused = {"background-color": "rgb(140, 73, 235)", "color": "white"};
+  const nonePaused = { "background-color": "rgb(140, 73, 235)", "color": "white", "opacity": "0.3" };
+  const nonePausedFiltered = { "background-color": "rgba(60,46,109)", "color": "rgb(89, 98, 109)", "border-color": "white", "border-style": "solid" };
+  const somePaused = { "background-color": "rgb(140, 73, 235)", "color": "white" };
+  const somePausedFiltered = { "background-color": "rgb(140, 73, 235)", "color": "white", "border-color": "white", "border-style": "solid" };
   //Stale
   const noneStale = { "background-color": "rgb(204, 150, 49)", "color": "rgb(89, 98, 109)" };
+  const noneStaleFiltered = { "background-color": "rgb(204, 150, 49)", "color": "rgb(89, 98, 109)", "border-color": "white", "border-style": "solid" };
   const someStale = { "background-color": "rgb(255, 169, 8)", "color": "black" };
+  const someStaleFiltered = { "background-color": "rgb(255, 169, 8)", "color": "black", "border-color": "white", "border-style": "solid" };
   //Inactive
-  const noneInactive = { "background-color": "rgb(63, 78, 96)", "opacity": "0.3", };
+  const noneInactive = { "background-color": "rgb(63, 78, 96)", "opacity": "0.3" };
+  const noneInactiveFiltered = { "background-color": "rgb(63, 78, 96)", "opacity": "0.3", "border-color": "white", "border-style": "solid" };
   const someInactive = { "background-color": "rgb(63, 78, 96)", "color": "white" };
+  const someInactiveFiltered = { "background-color": "rgb(63, 78, 96)", "color": "white", "border-color": "white", "border-style": "solid" };
 
 
   //Styles for Heading display
@@ -77,16 +87,25 @@ export  default function Header(props) {
     console.log(statusFilters());
   };
 
+  const styler = (check, status, some, none, someFiltered, noneFiltered) => {
+    if ((check() && statusFilters().status !== status)
+    || (check() && statusFilters().status === status && !statusFilters().active)) return some;
+    if ((!check() && statusFilters().status !== status)
+    || (!check() && statusFilters().status === status && !statusFilters().active)) return none;
+    if (check() && statusFilters().status === status && statusFilters().active) return someFiltered;
+    else return noneFiltered;
+  }
+
   return (
     <header style={ !activeQuery() ? fullDisplay : halfDisplay}>
       <img src={logo} width='65pxvw' height='65px' class="closeModal" onclick={() => setShowModal(false)}></img>
       <h1 class="queries">{`${queries().length}`} queries</h1>
       <div style={infoContainer}>
       <nav class="statusGrid">
-        <div class="statusBtn" style={fresh() ? someFresh : noneFresh} onClick={setFresh}>fresh ({fresh()})</div>
-        <div class="statusBtn" style={loading() ? someLoading : noneLoading} onClick={setFetching}>fetching ({loading()})</div>
-        <div class="statusBtn" style={paused() ? somePaused : nonePaused} onClick={setPaused}>paused ({paused()})</div>
-        <div class="statusBtn" style={stale() ? someStale : noneStale} onClick={setStale}>stale ({stale()})</div>
+        <div class="statusBtn" style={styler('fresh', fresh, someFresh, noneFresh, someFreshFiltered, noneFreshFiltered)} onClick={setFresh}>fresh ({fresh()})</div>
+        <div class="statusBtn" style={styler('fetching', someLoading, noneLoading, someLoadingFiltered, noneLoadingFiltered)} onClick={setFetching}>fetching ({loading()})</div>
+        <div class="statusBtn" style={styler('paused', paused, somePaused, nonePaused, somePausedFiltered, nonePausedFiltered)} onClick={setPaused}>paused ({paused()})</div>
+        <div class="statusBtn" style={styler('stale', stale, someStale, noneStale, someStaleFiltered, noneStaleFiltered)} onClick={setStale}>stale ({stale()})</div>
         <div class="statusBtn" style={inactive() ? someInactive : noneInactive} onClick={setInactive}>inactive ({inactive()})</div>
       </nav>
       <div style={sortOptions}>
