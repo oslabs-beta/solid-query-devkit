b612105dba58da866fe8724f5bd43d99b6050984
Truett <truett.davis@gmail.com>
2023-06-10 19:29:43 -0400

    additional typescript changes to context.tsx

--- src/Context.tsx
+++ src/Context.tsx
@@ -14,17 +14,17 @@ type queryProviderPlus = queryProvider & JSX.Element
 export function QueryProvider (props: queryProviderPlus) {
   const queryClient: QueryClient = useQueryClient();
 
-  const [activeQuery, setActiveQuery] = createSignal();
+  const [activeQuery, setActiveQuery] = createSignal<any>();
   const [queries, setQueries] = createSignal<Query[]>([]);
-  const [sort, setSort] = createSignal({type: 'last-updated', reverse: false});
-  const [filter, setFilter] = createSignal({text: '', status: ''});
+  const [sort, setSort] = createSignal<{type: string, reverse: boolean}>({type: 'last-updated', reverse: false});
+  const [filter, setFilter] = createSignal<{text: string, status: string}>({text: '', status: ''});
 
   //showData & showModal
-  const [showModal, setShowModal] = createSignal(false);
+  const [showModal, setShowModal] = createSignal<boolean>(false);
 
 
   //subscribing to the query cache, which runs the function every time the queryCache updates 
-  // TD changed queryCache to getQueryCache() and queries to getAll() --> prior were set to private and inaccessible in TypeScript
+  // TD changed queryCache to getQueryCache() and queries to getAll() --> prior were private types and inaccessible in TypeScript
   queryClient.getQueryCache().subscribe(() => {
     setQueries(() => [...queryClient.getQueryCache().getAll()]);
     if (activeQuery()) {
